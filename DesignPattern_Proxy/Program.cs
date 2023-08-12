05205c0ca3cb2bda396f9e27df71eef27453bbcd
ruanheng <ruanheng1995@gmail.com>
2023-08-12 20:43:16 +0800

    移除 bin 和 obj 目录

---
+++ DesignPattern_Proxy/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DesignPattern_Proxy.Demo;
+
+Console.WriteLine("Hello, World!");
+
+
+SchollGirl mm = new SchollGirl();
+mm.Name = "小芳";
+
+Proxy proxy = new Proxy(mm);
+
+proxy.GiveDolls();
+proxy.GiveChocolate();
+proxy.GiveChocolate();
+
